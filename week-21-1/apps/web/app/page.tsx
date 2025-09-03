f8777d096ad899b4eeebab9827b941d02a528474
vishwakarma-vishal <vishal.vishwakarma.dev@gmail.com>
2025-09-03 19:42:12 +0530

    Started learning monorepo

--- week-21-1/apps/web/app/page.tsx
+++ week-21-1/apps/web/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
           </a>
         </div>
         <Button appName="web" className={styles.secondary}>
-          Open alert
+          Open alert menu
         </Button>
       </main>
       <footer className={styles.footer}>
